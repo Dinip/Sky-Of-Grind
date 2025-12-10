0b67085311d2dada4f080c9b3551f97c813a2c74
Dinis Pimpão <8941012+Dinip@users.noreply.github.com>
2025-12-10 21:27:15 +0000

    fix molten space time recipe in atomic forge

--- .minecraft/kubejs/server_scripts/mainlines/endgame.js
+++ .minecraft/kubejs/server_scripts/mainlines/endgame.js
@@ -13,11 +13,17 @@ sog.recipes.gtceu.electric_blast_furnace('inifnity')
 .duration(1400)
 .EUt(GTValues.VA[GTValues.OpV]);
 
+// replicate similar recipe without kerosene
 sog.recipes.gtceu.atomic_melting_module('molten_space_time')
 .perTick(true)
+.inputFluids('gtceu:hyper_ionized_helium_plasma 4')
+.inputFluids('gtceu:californite 58')
+.perTick(false)
 .itemInputs('4x gtceu:space_time_ingot')
+.chancedInput('2x gtceu:neutron_reflector', 3333, 0)
+.chancedInput('2x gtceu:neutron_reflector', 2500, 0)
+.chancedInput('2x gtceu:neutron_reflector', 1111, 0)
 .outputFluids('gtceu:molten_space_time 144')
 .duration(20*20)
 .EUt(GTValues.VA[GTValues.OpV]);
-
 })
